f2027b90ef61be7907b6986321619ebec5b4a527
Edilson Lima <edilson.lima1@hotmail.com>
2022-02-08 22:25:25 -0300

    Aula14-5

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
-import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import "./App.scss"
+
 
 function App() {
   return (
     <div>
-      <Header />
+      <Navbar />
     </div>
   );
 }
