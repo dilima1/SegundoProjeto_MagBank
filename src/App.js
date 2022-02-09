b2ec9bf8d553336fa1d8b461932ef5af27760211
Edilson Lima <edilson.lima1@hotmail.com>
2022-02-08 22:49:55 -0300

    Aula14-6

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 import "./App.scss"
 
 
-function App() {
+const App = () => {
   return (
     <div>
       <Navbar />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,41 @@
 import React from "react";
-import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
+import {
+  Navbar,
+  Nav,
+  Container,
+  ButtonGroup,
+  Button,
+  NavDropdown,
+} from "react-bootstrap";
 import "./Navbar.scss";
-
 
 const Navigation = () => (
   <Navbar bg="light" expand="lg">
     <Container>
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Brand href="#home">MAGBANK</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
-          </NavDropdown>
+          <Nav.Link href="#cartao">Cartão</Nav.Link>
+          <Nav.Link href="#quemsomos">Quem Somos</Nav.Link>
+          <Nav.Link href="#faq">FAQ</Nav.Link>
         </Nav>
+        <ButtonGroup aria-label="Basic example">
+          <Button variant="outline-secondary">
+            <NavDropdown title="Acessar minha conta" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Button>
+          <Button variant="outline-secondary">Abra sua conta</Button>
+        </ButtonGroup>
       </Navbar.Collapse>
     </Container>
   </Navbar>
